66776526c27aed90ed8c1cb11635d26dfb434be3
Oleksii Yakuba <alexeyyakuba@gmail.com>
2023-01-16 21:45:34 +0200

    js upd

--- src/js/app.js
+++ src/js/app.js
@@ -25,55 +25,114 @@ import * as flsFunctions from "./base/functions.js";
 // flsFunctions.menuInit();
 // flsFunctions.setAnimateDelay(0.07);
 // flsFunctions.fullVHfix();
+
+// Documentation: https://template.fls.guru/template-docs/modul-spojlery.html
+// Snippet (HTML): spollers
 // flsFunctions.spollers();
+
+// Documentation: https://template.fls.guru/template-docs/modul-taby.html
+// Snippet (HTML): tabs
 // flsFunctions.tabs();
+
+// Documentation: https://template.fls.guru/template-docs/modul-pokazat-eshhjo.html
+// Snippet (HTML): showmore
 // flsFunctions.showMore();
 
+// Documentation: https://template.fls.guru/template-docs/modul-ripple-effect.html
+// Snippet (HTML):
+// flsFunctions.rippleEffect();
+
+// Documentation: https://template.fls.guru/template-docs
+// Snippet (HTML):
+// flsFunctions.customCursor(true);
+
+// Documentation: https://template.fls.guru/template-docs/funkcional-popup.html
+// Snippet (HTML): pl, pop
 // import './libs/popup.js'
+
+// Documentation: https://template.fls.guru/template-docs/modul-animacii-parallaks-obektov-pri-dvizhenii-myshi.html
 // import './libs/parallax-mouse.js'
-// import './libs/fullpage.js'
-// import './libs/parallax.js'
 
 //===================== FORMS =====================//
 import * as flsForms from "./base/forms/forms.js";
 
-// flsForms.formFieldsInit({ viewPass: false });
+// Documentation: https://template.fls.guru/template-docs/rabota-s-formami.html
+// flsForms.formFieldsInit({
+// 	viewPass: false,
+// 	autoHeight: false,
+// });
+
 // flsForms.formSubmit();
 // flsForms.formQuantity();
 // flsForms.formRating();
 
 // import "./libs/select.js";
 // import "./base/forms/datepicker.js";
+
+// Plugin documentation: https://github.com/RobinHerbots/inputmask
 // import "./base/forms/inputmask.js";
+
+// Plugin documentation: https://refreshless.com/nouislider/
+// Snippet (HTML): range
 // import "./base/forms/range.js";
+
+// Plugin documentation: https://atomiks.github.io/tippyjs/
+// Snippet (HTML): tip (додає атрибут з підказкою для html тега)
 // import "./base/tippy.js";
 
 //===================== SLIDERS  =====================//
-import * as flsSliders from "./base/sliders.js";
 
-// flsSliders.initSliders();
+// Plugin documentation: https://swiperjs.com/
+// Snippet(HTML): swiper
+import "./base/sliders.js";
 
-//===================== SCROLL =====================//
+//===================== SCROLLING =====================//
+
+//--- SIMPLEBAR ---//
+// Plugin documentation: https://github.com/Grsmto/simplebar/tree/master/packages/simplebar
+// import './files/scroll/simplebar.js';
+
+//--- LAZYLOAD ---//
+// Working documentation in the template: https://template.fls.guru/template-docs/modul-lenivaya-podgruzka-lazy-loading.html
+// Plugin documentation: https://github.com/verlok/vanilla-lazyload
+// import './files/scroll/lazyload.js';
+
+//--- WATCHER ---//
+// Documentation: https://template.fls.guru/template-docs/modul-nabljudatel-za-poyavleniem-elementa-pri-skrolle.html
+// import './libs/watcher.js'
+
+//--- FULLPAGE ---//
+// Documentation: https://template.fls.guru/template-docs/modul-poekrannoj-prokrutki-stranicy-fullpage.html
+// import './libs/fullpage.js'
+
+//--- PARALLAX ---//
+// Documentation: https://template.fls.guru/template-docs/paralaks-pri-skroli.html
+// Сніппет(HTML):
+// import "./libs/parallax.js";
+
 import * as flsScroll from "./base/scroll/scroll.js";
 
+// Documentation: https://template.fls.guru/template-docs/modul-plavnoj-navigacii-po-stranice.html
 // flsScroll.pageNavigation();
+
+// Documentation: https://template.fls.guru/template-docs/modul-dobavleniya-klassov-k-shapke-pri-prokrutke-stranicy.html
 // flsScroll.headerScroll();
+
 // flsScroll.stickyBlock();
+
+// Documentation: https://template.fls.guru/template-docs/modul-animacii-cifrovogo-lichilnika.html
 // flsScroll.digitsCounter();
 
 // import './base/scroll/simplebar.js';
 
-//===================== LAZY-LOAD =====================//
-// import './base/scroll/lazyload.js';
-
-//===================== WATCHER =====================//
-// import './libs/watcher.js'
-
 //===================== GALLERY =====================//
+// Plugin documentation: https://www.lightgalleryjs.com/docs/
 // import "./base/gallery.js";
 
 //===================== OTHERS =====================//
-import "./libs/dynamic_adapt.js";
+// Documentation: https://template.fls.guru/template-docs/dinamicheskij-adaptiv.html
+// import "./libs/dynamic_adapt.js";
+
 // import './libs/wNumb.min.js';
 
 //===================== MY-CODE =====================//
