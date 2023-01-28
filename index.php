33fed3da70991faf525a5cf2f9d8af0d4cc40c3b
Oleksii Yakuba <alexeyyakuba@gmail.com>
2023-01-28 13:13:46 +0200

    main page 2 first sections done

--- index.php
+++ index.php
@@ -6,8 +6,30 @@
 <?php include('inc/header.php'); ?>
 
 <!-- MAIN -->
-<main class="page page_main">
-  <h1>test</h1>
+<main class="page page_home">
+  <section class="fh-section">
+    <div class="fh-section__container">
+      <div class="fh-section__inner">
+        <h1 class="fh-section__title">
+          <span>Mavericks</span>
+          <span>agency</span>
+        </h1>
+      </div>
+    </div>
+  </section>
+  <section class="short-about">
+    <div class="short-about__container">
+      <div class="short-about__inner">
+        <span class="short-about__title p1">Mavericks Agency є командою дизайнерів та розробників продуктів, котрі працюють задля отримання результату</span>
+        <a href="#" class="short-about__button button button_primary">Більше про компанію</a>
+      </div>
+    </div>
+  </section>
+  <section class="services">
+    <div class="services__container">
+
+    </div>
+  </section>
 </main>
 
 <!-- FOOTER -->
