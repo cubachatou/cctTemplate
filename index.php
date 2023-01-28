414080b292950549f55fa8244304f4c2b312a080
Oleksii Yakuba <alexeyyakuba@gmail.com>
2023-01-28 18:01:24 +0200

    services

--- index.php
+++ index.php
@@ -21,13 +21,45 @@
     <div class="short-about__container">
       <div class="short-about__inner">
         <span class="short-about__title p1">Mavericks Agency є командою дизайнерів та розробників продуктів, котрі працюють задля отримання результату</span>
-        <a href="#" class="short-about__button button button_primary">Більше про компанію</a>
+        <a href="javascript:void(0)" class="short-about__button button button_primary">Більше про компанію</a>
       </div>
     </div>
   </section>
   <section class="services">
     <div class="services__container">
-
+      <div class="services__inner">
+        <div class="services__heading">
+          <h2 class="services__title">Наші послуги</h2>
+          <span class="services__sub-title p6">Lorem ipsum</span>
+        </div>
+        <div class="services__grid">
+          <div class="services__card service-card">
+            <div class="service-card__content">
+              <span class="service-card__number text-red">01</span>
+              <h3 class="service-card__title">Дизайн</h3>
+              <span class="service-card__text p4">Той дизайн, який дійсно вирішує проблеми бізнесу і ваших клієнтів</span>
+            </div>
+            <a href="javascript:void(0)" class="service-card__button button button_underline">Деталі</a>
+          </div>
+          <div class="services__card service-card">
+            <div class="service-card__content">
+              <span class="service-card__number text-blue">02</span>
+              <h3 class="service-card__title">Розробка</h3>
+              <span class="service-card__text p4">Розробляємо працюючі рішення, без багів і костилів</span>
+            </div>
+            <a href="javascript:void(0)" class="service-card__button button button_underline">Деталі</a>
+          </div>
+          <div class="services__card service-card">
+            <div class="service-card__content">
+              <span class="service-card__number text-purple">03</span>
+              <h3 class="service-card__title">Підтримка</h3>
+              <span class="service-card__text p4">Нам чхати, який час доби чи день тижня – ми готові перефарбувати кнопку в інший колір</span>
+            </div>
+            <a href="javascript:void(0)" class="service-card__button button button_underline">Деталі</a>
+          </div>
+        </div>
+        <a href="javascript:void(0)" class="services__more-button button button_primary">Більше про послуги</a>
+      </div>
     </div>
   </section>
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,121 +1,71 @@
 module.exports = {
-	mode: "jit",
-	content: ["./src/**/*.{html,js,htm}"],
-	theme: {
-		content: {},
-		container: {
-			center: true,
-			padding: "4rem",
-		},
-		/*===================================<Breakpoints>===================================*/
-		screens: {
-			md4: "479.98px",
-			// => @media (min-width: 479.98px) { ... }
-			md3: "767.98px",
-			// => @media (min-width: 767.98px) { ... }
-			md2: "991.98px",
-			// => @media (min-width: 991.98px) { ... }
-			lg: "1199.98px",
-			// => @media (min-width: 1199.98px) { ... }
-			xl: "1439.98px",
-			// => @media (min-width: 1439.98px) { ... }
-		},
-		/*===================================<Colors>===================================*/
-		colors: {
-			inherit: "inherit",
-
-			primary: {
-				main: "#4F31E4",
-				surface: "#F7F5FF",
-				border: "#BDB1F6",
-				hovered: "#3A1BD1",
-				pressed: "#23117F",
-				focused: "#4F31E433",
-			},
-
-			media: {
-				main: "#0D86FF",
-				surface: "#E3F3FF",
-				border: "#AED7FF",
-				hovered: "#0B70D5",
-				pressed: "#074380",
-				focused: "#0B70D533",
-			},
-
-			additional: {
-				main: "#F7E474",
-				surface: "#FFFDE5",
-				border: "#FCF6D1",
-				hovered: "#CEBE61",
-				pressed: "#7C723A",
-				focused: "#F7E47433",
-			},
-
-			danger: {
-				main: "#E00000",
-				surface: "#FFEDED",
-				border: "#FFD6D6",
-				hovered: "#A30000",
-				pressed: "#630000",
-			},
-
-			warning: {
-				main: "#EA780E",
-				surface: "#FFF2E7",
-				border: "#FFCA99",
-				hovered: "#D16603",
-				pressed: "#653508",
-			},
-
-			success: {
-				main: "#008243",
-				surface: "#EBF5F0",
-				border: "#B3DAC7",
-				hovered: "#006836",
-				pressed: "#005029",
-			},
-
-			info: {
-				main: "#0B5CD7",
-				surface: "#ECF2FC",
-				border: "#B6CFF3",
-				hovered: "#044DBA",
-				pressed: "#043C8F",
-			},
-
-			neutral: {
-				10: "#FFFFFF",
-				20: "#F5F5F5",
-				30: "#EDEDED",
-				40: "#E0E0E0",
-				50: "#C2C2C2",
-				60: "#9E9E9E",
-				70: "#757575",
-				80: "#616161",
-				90: "#424242",
-				100: "#0A0A0A",
-			},
-		},
-		/*===================================<Extend>===================================*/
-		extend: {
-			fontFamily: {
-				sans: ["Inter", "sans-serif"],
-				montserrat: ["Montserrat", "sans-serif"],
-			},
-			spacing: {},
-			borderRadius: {},
-			borderWidth: {},
-			gap: {},
-			outlineWidth: {},
-			maxWidth: {},
-			minWidth: {},
-			minHeight: {},
-			fontSize: {},
-			letterSpacing: {},
-			dropShadow: {},
-			backgroundImage: {},
-		},
-		/*===================================<Plugins>===================================*/
-	},
-	plugins: [],
+  mode: "jit",
+  content: ["./src/**/*.vue", "index.php", "header.php", "footer.php", "home.php", "contacts.php", "about.php", "404.php", "inc/**/*.php"],
+  theme: {
+    content: {},
+    // container: {
+    //   center: true,
+    //   padding: "4rem",
+    // },
+    /*===================================<Breakpoints>===================================*/
+    screens: {
+      md4: "479.98px",
+      // => @media (min-width: 479.98px) { ... }
+      md3: "767.98px",
+      // => @media (min-width: 767.98px) { ... }
+      md2: "991.98px",
+      // => @media (min-width: 991.98px) { ... }
+      lg: "1199.98px",
+      // => @media (min-width: 1199.98px) { ... }
+      xl: "1439.98px",
+      // => @media (min-width: 1439.98px) { ... }
+    },
+    /*===================================<Colors>===================================*/
+    colors: {
+      inherit: "inherit",
+      primary: {
+        main: "#00aec5",
+        shadow: "#008B9E",
+      },
+      turquoise: "#40E2E2",
+      blue: "#6DC8FF",
+      purple: "#AD86F5",
+      red: "#E96283",
+      orange: "#FA905E",
+      yellow: "#FCCD20",
+      neutral: {
+        10: "#FFFFFF",
+        20: "#eeeeef",
+        30: "#e8e8e9",
+        40: "#dcddde",
+        50: "#c3c4c6",
+        60: "#a4a5a9",
+        70: "#808288",
+        80: "#6f7279",
+        90: "#555860",
+        100: "#1c202c",
+      },
+    },
+    /*===================================<Extend>===================================*/
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      spacing: {},
+      borderRadius: {},
+      borderWidth: {},
+      gap: {},
+      outlineWidth: {},
+      maxWidth: {},
+      minWidth: {},
+      minHeight: {},
+      fontSize: {},
+      letterSpacing: {},
+      dropShadow: {},
+      backgroundImage: {},
+    },
+    /*===================================<Plugins>===================================*/
+  },
+  plugins: [],
 };
