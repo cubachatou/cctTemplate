1a85a7b074c91b291dae0e5b2076072066843311
Oleksii Yakuba <alexeyyakuba@gmail.com>
2023-02-04 15:25:44 +0200

    folio in progress

--- portfolio.php
+++ portfolio.php
@@ -16,9 +16,49 @@
           </svg>
         </span>
         Назад</a>
-      <h1 class="page-heading__title">Контакти</h1>
+      <h1 class="page-heading__title">Наше портфоліо</h1>
     </div>
   </section>
+  <div class="folio-grid">
+    <div class="folio-grid__item folio-item">
+      <div class="folio-item__image -ibg_contain">
+        <img src="images/cases/Gulliver.jpg" alt="Gulliver">
+      </div>
+      <div class="folio-item__content">
+        <h2 class="folio-item__title h4">Gulliver</h2>
+        <span class="folio-item__sub-title capt-1">Дизайн сайту для торгово-розважального і бізнес-центру Gulliver</span>
+      </div>
+    </div>
+  </div>
+  <nav class="paggination">
+    <button type="button" class="paggination__arrow paggination__arrow__prev icon-wrapper">
+      <svg class="icon icon-arrow">
+        <use xlink:href="/images/svgMap.svg#icon-arrow"></use>
+      </svg>
+    </button>
+    <ul class="paggination__list">
+      <li class="paggination__item">
+        <a href="javascript:void(0)" class="paggination__link">1</a>
+      </li>
+      <li class="paggination__item">
+        <a href="javascript:void(0)" class="paggination__link">2</a>
+      </li>
+      <li class="paggination__item">
+        <a href="javascript:void(0)" class="paggination__link">3</a>
+      </li>
+      <li class="paggination__item">
+        <a href="javascript:void(0)" class="paggination__link">4</a>
+      </li>
+      <li class="paggination__item">
+        <a href="javascript:void(0)" class="paggination__link">5</a>
+      </li>
+    </ul>
+    <button type="button" class="paggination__arrow paggination__arrow__next icon-wrapper">
+      <svg class="icon icon-arrow rotate-180">
+        <use xlink:href="/images/svgMap.svg#icon-arrow"></use>
+      </svg>
+    </button>
+  </nav>
 </main>
 
 <!-- FOOTER -->
